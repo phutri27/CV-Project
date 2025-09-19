c22bc423c6d495ba1ffb50bd7cfe459c687ab1cd
phutri27 <triphu27@gmail.com>
2025-09-19 18:30:30 +0000

    Create personal information section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import { useState } from 'react'
+import { PersonInfo } from "./components/personalInfo";
+import { useState } from "react";
+function CV({person}){
+    return (
+        <div>
+            <div>
+                <p><b>Name: {person.name}</b></p>
+                <p><b>Age: {person.email}</b></p>
+                <p><b>Phone Number: {person.phone}</b></p>
+            </div>
+        </div>
+    )
+}
 
 function App() {
-  
+    const [person, setPerson] = useState({
+        name: "",
+        email: "",
+        phone: "",
+    })
+    return (
+      <div>
+        <PersonInfo person={{...person}}
+        setPerson={setPerson}/>
+        
+        <CV person={{...person}}/>
+
+      </div>
+    )
 }
 
 export default App
